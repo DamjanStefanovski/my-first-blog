39a15e73cd9f4c51f877014702d9d53d55c3f159
DamjanStefanovski <damjan.stefanovski@gmail.com>
2018-02-22 13:57:02 -0500

    CSS and post.html template added

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,5 +4,9 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
+#create a variable for our QuerySet: posts.
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+
+#render function we have one parameter request (everything we receive from the user via the Internet) and another giving the template file ('blog/post_list.html').
     return render(request, 'blog/post_list.html', {'posts': posts})
