79119c2aa0884144d35ba970feed5af32ad380b5
DamjanStefanovski <damjan.stefanovski@gmail.com>
2018-03-12 14:14:38 -0400

    Post Model Admin changed for an easy CRUD

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -12,11 +12,14 @@ class Post(models.Model):  #this line defines our model (it is an object)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE) # models.ForeignKey – this is a link to another model.     #defining the properties of title, text, created_date, published_date , author
     title = models.CharField(max_length=200)
     text = models.TextField()
+    content = models.TextField()
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
-    def publish(self):  #this is a function/method and publish is the name of the method
-        self.published_date = timezone.now()
+    def __unicode__(self):  #this is a function/method and publish is the name of the method
+        self.title
         self.save()
 #indent methods inside the class
     def __str__(self):
