e47944f7ca3b4025baf0cfb4b3d8c4c1a825067e
DamjanStefanovski <damjan.stefanovski@gmail.com>
2018-03-25 15:35:58 -0400

    Just an update

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from . models import Post
 # Register your models here.
 
 class PostModelAdmin(admin.ModelAdmin):
@@ -15,3 +15,18 @@ class PostModelAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Post, PostModelAdmin)
+
+
+
+
+
+# Few lines for CRUD inside django shell -  Don't forget to import the model first and User
+
+#from blog.models import Post  //// from django.contrib.auth.models import User ///
+#me = User.objects.get(username='damjan') /// from django.utils import timezone ///
+# Post.objects.all() ///  Post.objects.create(author=me, title='Sample title', text='Test')
+# User.objects.all() // Post.objects.filter(author=me)
+#  Post.objects.filter(title__contains='any variable')
+# Post.objects.filter(published_date__lte=timezone.now())
+# Post.objects.order_by('created_date') //
+# Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
